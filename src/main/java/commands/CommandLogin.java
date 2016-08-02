894d4b108f9284653cf368b9ef7e267978cf5a14
dconradt <r8buffalo>
2016-08-01 18:12:10 -0700

    added comments

--- src/main/java/commands/CommandLogin.java
+++ src/main/java/commands/CommandLogin.java
@@ -8,6 +8,9 @@ import java.io.IOException;
 /**
  * Created by Dan on 7/23/2016.
  * Takes args[0]-host, arg[1]-port, arg[2]-username, arg[3]-pw
+ * Connects to server
+ * Logs in
+ * Sets the mode to passive mode.
  */
 public class CommandLogin implements Command {
 
