1b83d0d12ae986f66824a0dbcf6f9f718ad77d03
Simone Talla <simonetalla@yahoo.fr>
2016-08-06 11:53:48 -0700

    Merge branch 'develop' of https://github.com/lou-ftp/lou-ftp into develop
    # Conflicts:
    #	src/main/java/commands/AddFileToRemoteServer.java

--- src/main/java/commands/CommandGetMultiple.java
+++ src/main/java/commands/CommandGetMultiple.java
@@ -13,16 +13,26 @@ public class CommandGetMultiple implements Command {
     public void execute(FTPClient client, String... args) throws IOException {
 
         Scanner userChoice = new Scanner(System.in);
-        String yourChoice;
+        String yourChoice = null;
+        AddFileToRemoteServer addFile = new AddFileToRemoteServer();
 
-        FileOutputStream stream = new FileOutputStream(args[1]);
+        FileOutputStream stream;/* = new FileOutputStream(args[1]);
         if (!client.retrieveFile(args[0], stream)) {
             System.out.println("Could not find file " + args[0] + " or failed to retrieve for other reasons");
         }
         stream.flush();
-        stream.close();
+        stream.close();*/
 
         do {
+
+           /* stream = new FileOutputStream(args[1]);
+            if (!client.retrieveFile(args[0], stream)) {
+                System.out.println("Could not find file " + args[0] + " or failed to retrieve for other reasons");
+            }
+            stream.flush();
+            stream.close();*/
+
+            addFile.execute(client, args);
 
             System.out.println("Would you like to add another file?");
             System.out.println("Yes . . . . . . . . . . . (1)");
@@ -30,13 +40,6 @@ public class CommandGetMultiple implements Command {
 
             yourChoice = userChoice.next();
 
-
-            stream = new FileOutputStream(args[1]);
-            if (!client.retrieveFile(args[0], stream)) {
-                System.out.println("Could not find file " + args[0] + " or failed to retrieve for other reasons");
-            }
-            stream.flush();
-            stream.close();
 
         }while(!yourChoice.equals("2"));
 
