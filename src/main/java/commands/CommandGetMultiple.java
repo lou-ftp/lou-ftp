4a9e001cd8e5786d07c72f2fef7a48e3794e5310
Andrew Wen <2060andrew@gmail.com>
2016-08-09 18:16:09 -0700

    Fix getMultiple to actually...get multiple

--- src/main/java/commands/CommandGetMultiple.java
+++ src/main/java/commands/CommandGetMultiple.java
@@ -4,23 +4,25 @@ package commands;
 
 import org.apache.commons.net.ftp.FTPClient;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class CommandGetMultiple implements Command {
     public void execute(FTPClient client, String... args) throws IOException {
 
-        Scanner userChoice = new Scanner(System.in);
-        String yourChoice = null;
-        AddFileToRemoteServer addFile = new AddFileToRemoteServer();
-        FileOutputStream stream;
+        String yourChoice;
         do {
-            addFile.execute(client, args);
+            Scanner userChoice = new Scanner(System.in);
+            System.out.println("Please input the path of the file you wish to download");
+            String source = userChoice.nextLine();
+            System.out.println("Please input the path to save the file");
+            String target = userChoice.nextLine();
+            CommandGetFile getFile = new CommandGetFile();
+            getFile.execute(client, source, target);
             System.out.println("Would you like to get another file?");
             System.out.println("Yes . . . . . . . . . . . (1)");
             System.out.println("No . . . . . . . . . . .  (2)");
             yourChoice = userChoice.next();
-        }while(!yourChoice.equals("2"));
+        } while (!yourChoice.equals("2"));
     }
 }
